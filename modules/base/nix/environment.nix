4d697776d9ad25e1d7175c6238a290f50cf3898b
isabel <isabel@isabelroses.com>
2024-10-02 16:54:19 +0100

    lol

--- modules/base/nix/environment.nix
+++ modules/base/nix/environment.nix
@@ -6,33 +6,35 @@
 }:
 let
   inherit (builtins) elem;
-  inherit (lib.trivial) pipe;
+  inherit (lib.lists) optionals;
   inherit (lib.attrsets) filterAttrs mapAttrs';
 in
 {
   environment = {
+    # git is required for flakes
+    systemPackages = [ pkgs.git ];
+
     # something something backwards compatibility something something nix channels
     etc =
       let
         inherit (config.nix) registry;
-        commonPaths = [
-          "self"
-          "nixpkgs"
-          "beapkgs"
-          "home-manager"
-        ];
+        flakes =
+          [
+            "nixpkgs"
+            "beapkgs"
+            "home-manager"
+          ]
+          ++ optionals pkgs.stdenv.hostPlatform.isDarwin [
+            "nix-darwin"
+          ];
       in
-      pipe registry [
-        (filterAttrs (name: _: (elem name commonPaths)))
-        (mapAttrs' (
-          name: value: {
-            name = "nix/path/${name}";
-            value.source = value.flake;
-          }
-        ))
-      ];
-
-    # git is required for flakes
-    systemPackages = [ pkgs.git ];
+      registry
+      |> filterAttrs (name: _: elem name flakes)
+      |> mapAttrs' (
+        name: value: {
+          name = "nix/path/${name}";
+          value.source = value.flake;
+        }
+      );
   };
 }
